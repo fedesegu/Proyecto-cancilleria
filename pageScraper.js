9bad57c065e3e1c6137af5290d0d8e25087437ce
Franco Berardi <darkwilly08@gmail.com>
2023-06-12 19:50:15 -0300

    feat: extracting html for chapters

--- pageScraper.js
+++ pageScraper.js
@@ -1,39 +1,57 @@
-const urlPrimaria = 'http://www.argentina-rree.com'
+const { writeFile } = require("./helpers/file");
+
+const initPage = async (browser, url) => {
+  console.log(`Navigating to ${url}...`);
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.waitForSelector("body");
+  return page;
+};
+
+const retrieveChapters = async (page) =>
+  page.$$eval("big", (elements) =>
+    elements.map((el) => ({
+      title: el.textContent.replace(/\s\s+/g, " ").trim(),
+      links: [...el.closest("p").nextElementSibling.querySelectorAll("a")].map(
+        (a) => `https://web.archive.org/web/20070513055106/${a.href}`
+      ),
+    }))
+  );
+
 const scraperObject = {
-	url: 'https://web.archive.org/web/20070513055106/http://www.argentina-rree.com/historia_indice01.htm',
-	async scraper(browser){
-		let page = await browser.newPage();
-		console.log(`Navigating to ${this.url}...`);
-		await page.goto(this.url);
-        // Wait for the required DOM to be rendered
-		await page.waitForSelector('body');
-		// Get the link to all the required books
-		let urls = await page.$$eval(' td > ul > li', links => {
-			// Extract the links from the data
-			links = links.map(el => el.querySelector('font > a').href);
-			return links;
-		});
-		// console.log(urls);
-		
-		
+  url: "https://web.archive.org/web/20070513055106/http://www.argentina-rree.com/historia_indice01.htm",
+  scraper: async (browser) => {
+    const { url } = scraperObject;
+    const indexPage = await initPage(browser, url);
 
-		// Loop through each of those links, open a new page instance and get the relevant data from them
-		let pagePromise = (link) => new Promise(async(resolve, reject) => {
-			let dataObj = {};
-			let newPage = await browser.newPage();
-			await newPage.goto(link);
-			dataObj['parrafos'] = await newPage.$eval('p', text => text.textContent);
-			resolve(dataObj);
-			await newPage.close();
-		});
+    const chapters = await retrieveChapters(indexPage);
 
-		for(link in urls){
-			let currentPageData = await pagePromise(urls[link]);
-			// scrapedData.push(currentPageData);
-			console.log(currentPageData);
-		}
-		
-	}
-}
+    console.log("chapters", chapters);
+
+    // TODO: tenes que usar chapters, te puso solo el primero a modo de prueba, pero te perdes el 
+    // resto de los chapters si lo dejas asi
+    for (chapter of [chapters[0]]) {
+      const chapterPages = await Promise.all(
+        chapter.links.map(async (link, pageNumber) => {
+          const chapterPage = await initPage(browser, link);
+
+          return {
+            title: chapter.title,
+            pageNumber,
+            body: await chapterPage.evaluate(() =>
+              document.querySelector("body").innerHTML.trim()
+            ),
+          };
+        })
+      );
+
+      // TODO: dsp de guardar, podes crear un html nuevo que tenga todos ls links a los capitulos
+      // o podes concatenar todos los capitulos en un solo html y guardar ese finalmente
+      chapterPages.forEach((page) => {
+        writeFile(`./${page.title}_${page.pageNumber}.html`, page.body);
+      });
+    }
+  },
+};
 
 module.exports = scraperObject;
